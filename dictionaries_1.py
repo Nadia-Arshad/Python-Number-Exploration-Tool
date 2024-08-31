a247e273377a28fa9bfb7ab19b0f2d77193e7a63
Nadia1379 <nadiaarshad1988@gmail.com>
2024-08-31 23:12:02 +0500

    Number Exploration Tools - Assignment/Practice

---
+++ dictionaries_1.py
@@ -0,0 +1,26 @@
+# alien_0 = {"color": "red", "score": 9}
+# print(alien_0)
+# print(alien_0["color"])
+# print(alien_0["score"])
+# alien_0['x position'] = 0
+# alien_0['y position'] = 25
+# print(alien_0)
+# alien_0["name"] = "nadia"
+# print(alien_0)
+
+# favorite_numbers = {"nadia": 2, "arshad": 7, "manahil": 13, "madi": 10}
+# for favorite in favorite_numbers.values():
+#     print(favorite)
+
+programming_words = {
+    "append": "add to the end",
+    "pop": "delete from the end",
+    "remove": "remove the element with the index",
+    "title": "capitalizes the first letters",
+    "lower": "make all letters lower",
+    }
+
+neatly_written = {programming_words["append"].title()}
+print(f"append :\n {neatly_written}")
+
+
